1b9071a9ec7bdbd4b9da2a375bb2ebdd171c9db8
Your Name <croissant3100@gmail.com>
2022-04-07 17:52:30 +0900

    firstcommit

---
+++ lib/detail_page.dart
@@ -0,0 +1,19 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          child: Text('動画'),
+        ),
+      ),
+    );
+  }
+}
